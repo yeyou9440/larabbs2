d098b3ca94c88f609c6ac6f80bcb74f9ba4185d1
vagrant <vagrant@homestead>
2019-04-16 03:13:28 +0000

    reply port

--- resources/views/replies/show.blade.php
+++ resources/views/replies/show.blade.php
@@ -1,42 +1,77 @@
 @extends('layouts.app')
 
+@section('title', $topic->title)
+@section('description', $topic->excerpt)
+
 @section('content')
 
-<div class="container">
-  <div class="col-md-10 offset-md-1">
-    <div class="card ">
-      <div class="card-header">
-        <h1>Reply / Show #{{ $reply->id }}</h1>
-      </div>
+  <div class="row">
 
-      <div class="card-body">
-        <div class="card-block bg-light">
-          <div class="row">
-            <div class="col-md-6">
-              <a class="btn btn-link" href="{{ route('replies.index') }}"><- Back</a>
-            </div>
-            <div class="col-md-6">
-              <a class="btn btn-sm btn-warning float-right mt-1" href="{{ route('replies.edit', $reply->id) }}">
-                Edit
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
+      <div class="card ">
+        <div class="card-body">
+          <div class="text-center">
+            作者：{{ $topic->user->name }}
+          </div>
+          <hr>
+          <div class="media">
+            <div align="center">
+              <a href="{{ route('users.show', $topic->user->id) }}">
+                <img class="thumbnail img-fluid" src="{{ $topic->user->avatar }}" width="300px" height="300px">
               </a>
             </div>
           </div>
         </div>
-        <br>
-
-        <label>Topic_id</label>
-<p>
-	{{ $reply->topic_id }}
-</p> <label>User_id</label>
-<p>
-	{{ $reply->user_id }}
-</p> <label>Content</label>
-<p>
-	{{ $reply->content }}
-</p>
       </div>
     </div>
-  </div>
-</div>
 
-@endsection
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
+      <div class="card">
+        <div class="card-body">
+          <h1 class="text-center mt-3 mb-3">
+            {{ $topic->title }}
+          </h1>
+
+          <div class="article-meta text-center text-secondary">
+            {{ $topic->created_at->diffForHumans() }}
+            ⋅
+            <i class="far fa-comment"></i>
+            {{ $topic->reply_count }}
+          </div>
+
+          <div class="topic-body mt-4 mb-4">
+            {!! $topic->body !!}
+          </div>
+
+          @can('update', $topic)
+            <div class="operate">
+              <hr>
+              <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                <i class="far fa-edit"></i> 编辑
+              </a>
+              <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
+                    style="display: inline-block;"
+                    onsubmit="return confirm('您确定要删除吗？');">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                  <i class="far fa-trash-alt"></i> 删除
+                </button>
+              </form>
+            </div>
+          @endcan
+
+        </div>
+      </div>
+
+      {{-- 用户回复列表 --}}
+      <div class="card topic-reply mt-4">
+          <div class="card-body">
+              @include('topics._reply_box', ['topic' => $topic])
+              @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+          </div>
+      </div>
+
+    </div>
+  </div>
+@stop
