011cb52ba635e4cdd5b7182167789465d3f4b461
vagrant <vagrant@homestead>
2019-04-16 14:43:47 +0000

    notifications list

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
         }
         $this->laravelNotify($instance);
     }
+
+    public function markAsRead(){
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
