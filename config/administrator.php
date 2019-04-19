6b66277c51231203d416ad7fe045b30877cc30e8
vagrant <vagrant@homestead>
2019-04-19 16:14:37 +0000

    site config 123232

--- config/administrator.php
+++ config/administrator.php
@@ -81,7 +81,7 @@ return array(
     'back_to_site_path' => '/',
 
     // 当选项 `permission` 权限检测不通过时，会重定向用户到此处设置的路径
-    'login_path' => 'login',
+    'login_path' => 'permission-denied',
 
     // 允许在登录成功后使用 Session::get('redirect') 将用户重定向到原本想要访问的后台页面
     'login_redirect_key' => 'redirect',
